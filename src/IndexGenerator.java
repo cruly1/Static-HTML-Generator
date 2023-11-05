b5e99c11cab6daea4562dcd08c75982bb716e701
cruly <pavai.viktor2@gmail.com>
2023-11-05 16:52:33 +0100

    html

--- src/IndexGenerator.java
+++ src/IndexGenerator.java
@@ -16,16 +16,7 @@ public class IndexGenerator {
     public void generateIndex(String rootPath) {
         StringBuilder sb = new StringBuilder();
 
-        sb.append("""
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>Static HTML Generator - Project</title>
-                </head>
-                <body>""");
+        sb.append(Utils.htmlHead());
         sb.append(htmlBody(rootPath));
         sb.append("""
                 </body>
